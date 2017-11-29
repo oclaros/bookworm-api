061160a3b7953710f64c91d8cd0480372dfd3874
Oscar Claros <Oscar Claros>
2017-11-29 15:13:49 -0600

    added api/auth route

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import express from 'express';
-import path from 'path';
+import express from "express";
+import path from "path";
+import mongoose from "mongoose";
+import bodyParser from 'body-parser';
+
+import auth from "./routes/auth";
 
 const app = express();
+app.use(bodyParser.json());
+mongoose.connect("mongodb://localhost/bookworm", { useMongoClient: true });
 
-
+app.use('/api/auth', auth);
+/*
 app.post('/api/auth', (req, res) => {
     res.status(400).json({errors: {global: "Invalid credentials"}});
 });
+*/
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
---
+++ src/models/User.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, lowercase: true, index: true },
+    passwordHash: { type: String, required: true }
+  },
+  { timestamp: true }
+);
+
+export default mongoose.model("User", schema);
